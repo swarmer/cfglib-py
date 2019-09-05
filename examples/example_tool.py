0dbcbb8f94a0c39d139ada96fde4e51ca7b2c0fb
Anton Barkovsky <anton@swarmer.me>
2019-09-05 17:08:14 +0300

    Update example tool

---
+++ examples/example_tool.py
@@ -0,0 +1,78 @@
+import argparse
+import json
+import sys
+
+import cfglib
+from cfglib.sources.env import EnvConfig
+
+
+# Config definitions
+# These will normally be in a separate module,
+# to be imported by the rest of the project like:
+# from xyz.config import cfg
+def parse_config_file(config_file_path) -> cfglib.DictConfig:
+    with open(config_file_path) as config_file:
+        return cfglib.DictConfig(json.load(config_file))
+
+
+default_config = cfglib.DictConfig({
+    'message': 'Hello!',
+    'config_file': None,
+})
+cfg = cfglib.CompositeConfig([
+    default_config,
+    EnvConfig(prefix='EXAMPLE_'),
+])
+
+
+# Main program
+def parse_cmdline_arguments() -> dict:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--config-file', type=str,
+        required=False, default=None,
+        help='Path to a json-encoded config file',
+    )
+    parser.add_argument(
+        '--message', type=str,
+        required=False, default=None,
+        help='The message that will be printed',
+    )
+
+    parsed_args = parser.parse_args()
+    config_items = {
+        k: v
+        for k, v in parsed_args.__dict__.items()
+        if v is not None
+    }
+    return config_items
+
+
+def initialize_config():
+    cmdline_config = cfglib.DictConfig(parse_cmdline_arguments())
+    # Command line arguments shoule be highest priority, so append them to the end
+    cfg.subconfigs.append(cmdline_config)
+
+    if cfg.get('config_file'):
+        # Load configuration from a config file, if specified
+        # Config file's priority is below that of cmdline args, so insert at position 2
+        cfg.subconfigs.insert(2, parse_config_file(cfg['config_file']))
+
+    # With the additional configuration sources injected, the end priority order
+    # from lowest to highest priority is now:
+    # - defaults
+    # - environment variables
+    # - config file
+    # - command line arguments
+
+
+def main():
+    initialize_config()
+    print(f'Config: {cfg}', file=sys.stderr)
+
+    message = cfg['message']
+    print(message)
+
+
+if __name__ == '__main__':
+    main()
